1334d8e07e91527787b7c7967e5f6b652c6e34aa
Tiago Andrade <tiagomontes.andrade@gmail.com>
2024-02-14 16:31:51 -0300

    Primeiro delivery de development

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -29,7 +29,7 @@ def hello():
     Returns:
         A string with the words 'Hello World!'.
     """
-    return "Hello World - LAB Final - DEVOPS - Versao 1.0!"
+    return "Hello World - LAB Final - DEVOPS - Versao 1.0 - Develop Env!"
 
 
 if __name__ == "__main__":
