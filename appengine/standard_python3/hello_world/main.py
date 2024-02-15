eb97db46fe4b9a54680772822cd7a667a63546c7
Tiago Andrade <tiagomontes.andrade@gmail.com>
2024-02-15 15:02:30 -0300

    Cloudbuild up

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -1,3 +1,4 @@
+print("Hello World!")
 # Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,28 +15,28 @@
 
 # [START gae_python38_app]
 # [START gae_python3_app]
-from flask import Flask
+#from flask import Flask
 
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
-app = Flask(__name__)
+#app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    """Return a friendly HTTP greeting.
+#@app.route("/")
+#def hello():
+#    """Return a friendly HTTP greeting.
+#
+#    Returns:
+#        A string with the words 'Hello World!'.
+#    """
+#    return "Hello World - LAB Final - DEVOPS - Versao 1.0!"
 
-    Returns:
-        A string with the words 'Hello World!'.
-    """
-    return "Hello World - LAB Final - DEVOPS - Versao 1.0!"
 
-
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. You
     # can configure startup instructions by adding `entrypoint` to app.yaml.
-    app.run(host="127.0.0.1", port=8080, debug=True)
+#    app.run(host="127.0.0.1", port=8080, debug=True)
 # [END gae_python3_app]
 # [END gae_python38_app]
