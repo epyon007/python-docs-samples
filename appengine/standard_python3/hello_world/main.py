e042f644b8d62d41d8a0b5cefb9b278706099cec
Tiago Andrade <tiagomontes.andrade@gmail.com>
2024-02-15 15:49:40 -0300

    Cloudbuild up

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -1,4 +1,4 @@
-print("Hello World! Caraio! - LAB Final - DEVOPS - Versao 1.0!")
+print("Hello World! Caraio! - LAB Final - DEVOPS - Versao 1.1!")
 # Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
