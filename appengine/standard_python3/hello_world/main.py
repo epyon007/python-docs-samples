9957794cea191225392890af40bd5b13ec5ec802
Tiago Andrade <tiagomontes.andrade@gmail.com>
2024-02-15 13:27:07 -0300

    Update Cloudbuild

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -14,12 +14,12 @@
 
 # [START gae_python38_app]
 # [START gae_python3_app]
-from flask import Flask
+from flask import flask
 
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
-app = Flask(__name__)
+app = flask(__name__)
 
 
 @app.route("/")
