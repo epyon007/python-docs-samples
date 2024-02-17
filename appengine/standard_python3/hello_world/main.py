e278777c4047fe7bcf69365c88711ef4f973b481
Tiago Andrade <tiagomontes.andrade@gmail.com>
2024-02-17 14:41:58 -0300

    main.py update

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -30,7 +30,7 @@ def hello():
     Returns:
         A string with the words 'Hello World!'.
     """
-    return "Hello World - LAB Final - DEVOPS - Versao 1.2!"
+    return "Hello World - LAB Final - DEVOPS - Versao 1.3!"
 
 
 #if __name__ == "__main__":
